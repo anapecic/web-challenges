0e196d079831c1c8fa7e2726e12cccdd2516855d
Ana Pecic <ana.pecic97@gmail.com>
2024-07-05 18:17:46 +0200

    finished tasks

---
+++ js-functions-2_arrow-functions/js/index.js
@@ -0,0 +1,85 @@
+console.clear();
+
+/*
+Rewrite the following functions as arrow functions.
+Use implicit returns when possible
+*/
+
+// function getCurrentHour() {
+//   const now = new Date();
+//   const currentHour = now.getHours();
+//   if (currentHour === 0) {
+//     return "12am";
+//   } else if (currentHour === 12) {
+//     return "12pm";
+//   } else if (currentHour <= 11) {
+//     return currentHour + "am";
+//   } else {
+//     return currentHour - 12 + "pm";
+//   }
+// }
+
+const getCurrentHour = () => {
+  const now = new Date();
+  const currentHour = now.getHours();
+  if (currentHour === 0) {
+    return "12am";
+  } else if (currentHour === 12) {
+    return "12pm";
+  } else if (currentHour <= 11) {
+    return currentHour + "am";
+  } else {
+    return currentHour - 12 + "pm";
+  }
+};
+
+console.log(getCurrentHour());
+
+// function getVectorLength(x, y, z) {
+//   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+// }
+
+const getVectorLength = (x, y, z) => (x ** 2 + y ** 2 + z ** 2) ** 0.5;
+console.log(getVectorLength(1, 2, 3));
+
+// function cleanInput(string) {
+//   return string.toLowerCase().trim();
+// }
+
+const cleanInput = (string) => string.toLowerCase().trim();
+
+console.log(cleanInput("Hello"));
+
+/*
+Rewrite the following arrow functions as classic functions.
+*/
+
+// const isOddAndSmall = (number) => {
+//   if (number > 10) {
+//     return false;
+//   }
+
+//   if (number % 2 === 0) {
+//     return false;
+//   }
+
+//   return true;
+// };
+
+const isOddAndSmall = (number) => {
+  if (number > 10) {
+    return false;
+  }
+
+  if (number % 2 === 0) {
+    return false;
+  }
+
+  return true;
+};
+
+console.log(isOddAndSmall(30));
+
+const add3 = (a, b, c) => a + b + c;
+
+const repeat10 = (string) => string.repeat(10);
