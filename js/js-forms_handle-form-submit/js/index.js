1a612b1ee60d4010097bb2e3b6d83cafc33a4c6f
Ana Pecic <ana.pecic97@gmail.com>
2024-07-10 11:06:10 +0200

    blabla

--- js/js-forms_handle-form-submit/js/index.js
+++ js/js-forms_handle-form-submit/js/index.js
@@ -5,11 +5,10 @@ const form = document.querySelector('[data-js="form"]');
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData);
-  console.log(data);
-  console.log(
-    Number(event.target.age.value) + Number(event.target.badness.value)
-  );
+  const data = Object.fromEntries(formData); //hier entsteht object, dadurch können wir auf alles mit variable zugreifen statt event.target
+  console.log(data.age); //mit data.age greife ich auf object zu und hier wird direkt der value ausgespuckt
+  console.log(data.badness);
+  console.log(Number(data.age) + Number(data.badness));
   event.target.reset();
   event.target.elements.firstName.focus();
 });
