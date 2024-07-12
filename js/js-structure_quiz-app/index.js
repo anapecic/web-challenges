ba8cbbd6a4db72a16338c010bab977a8a267e9bd
Ana Pecic <ana.pecic97@gmail.com>
2024-07-12 15:59:01 +0200

    odifhg

---
+++ js/js-structure_quiz-app/index.js
@@ -0,0 +1,9 @@
+import { cards } from "./lib/data.js";
+import { Header } from "./components/Header/Header.js";
+import { Form, handleFormSubmit } from "./components/Form/Form.js";
+import { Bookmark } from "./components/Bookmark/Bookmark.js";
+import { Card } from "./components/Card/Card.js";
+import { CardList } from "./components/CardList/CardList.js";
+import { App, render } from "./components/App/App.js";
+
+render();
