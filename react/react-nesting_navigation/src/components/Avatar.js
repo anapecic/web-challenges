7243dee65601cb1eb88c40c956a3d903ed43d917
Ana Pecic <ana.pecic97@gmail.com>
2024-07-22 12:51:22 +0200

    minor changes

--- react/react-nesting_navigation/src/components/Avatar.js
+++ react/react-nesting_navigation/src/components/Avatar.js
@@ -12,17 +12,3 @@ export default function Avatar() {
     </button>
   );
 }
-
-//Frage: Warum funktioniert das so nicht? Alternativ so funktioniert auch nicht: ""./img/avatar.jpg""
-
-// export default function Avatar() {
-//     return (
-//       <button
-//         type="button"
-//         onClick={() => console.log("I could toggle a profile!")}
-//         aria-label="toggle profile"
-//       >
-//         <Image alt="avatar" src="../img/avatar.jpg" />
-//       </button>
-//     );
-//   }
