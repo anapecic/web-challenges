887937ca87815e5a965b330302042b7f3b0231ff
Ana Pecic <ana.pecic97@gmail.com>
2024-07-26 11:05:59 +0200

    styled components added

---
+++ css/react-styled-components-1_styled-button-cra/src/App.js
@@ -0,0 +1,14 @@
+import StyledButton from "./components/StyledButton";
+import { PrimaryButton } from "./components/PrimaryButton";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello from React</h1>
+      <StyledButton>Default Button </StyledButton>
+      <PrimaryButton>Primary Button</PrimaryButton>
+    </main>
+  );
+}
+
+export default App;
---
+++ css/react-styled-components-1_styled-button-cra/src/components/PrimaryButton.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const PrimaryButton = styled.button`
+  background: blue;
+  color: lightblue;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border: none;
+`;
